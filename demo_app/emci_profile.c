02119e2d535e572ed1957cabcccc28a8ecb1c6d4
Mikhail Ustinskov <ustinskov@yandex.ru>
2023-05-15 01:38:04 +0300

    Convert tabs to spaces

--- demo_app/emci_profile.c
+++ demo_app/emci_profile.c
@@ -8,73 +8,73 @@ float cmd_var_delay = 888.8;
 
 const emci_command_t cmd_array[] =
 {
-	{"about", about_handler, "", 0,
-	NULL,
-	"Display version information", "<strA>\0strB\0<strC>\0strD"},
+    {"about", about_handler, "", 0,
+    NULL,
+    "Display version information", "<strA>\0strB\0<strC>\0strD"},
 
-	{"setpoint", emci_var_handler, "u", 1,
-	&(emci_var_handler_data_t){.var=&cmd_var_setpoint, .min={EMCI_ARG('u'), {.u=100}}, .max={EMCI_ARG('u'),{.u=200}}},
-	"Display/change setpoint value", "value"},
+    {"setpoint", emci_var_handler, "u", 1,
+    &(emci_var_handler_data_t){.var=&cmd_var_setpoint, .min={EMCI_ARG('u'), {.u=100}}, .max={EMCI_ARG('u'),{.u=200}}},
+    "Display/change setpoint value", "value"},
 
-	{"delay", emci_var_handler, "f", 1,
-	&(emci_var_handler_data_t){.var=&cmd_var_delay, .min={EMCI_ARG('f'), {.f=0.0}}, .max={EMCI_ARG('f'), {.f=60.0}}, .prec=1},
-	"Display/change delay", "delay[ms]"},
+    {"delay", emci_var_handler, "f", 1,
+    &(emci_var_handler_data_t){.var=&cmd_var_delay, .min={EMCI_ARG('f'), {.f=0.0}}, .max={EMCI_ARG('f'), {.f=60.0}}, .prec=1},
+    "Display/change delay", "delay[ms]"},
 
-	{"fff", emci_var_handler, "f", 1,
-	&(emci_var_handler_data_t){.var=&cmd_var_fff, .min={EMCI_ARG('f'), {.f=.1}}, .max={EMCI_ARG('f'), {.f=.9}}, .prec=4},
-	"Display/change fff value", "value"},
+    {"fff", emci_var_handler, "f", 1,
+    &(emci_var_handler_data_t){.var=&cmd_var_fff, .min={EMCI_ARG('f'), {.f=.1}}, .max={EMCI_ARG('f'), {.f=.9}}, .prec=4},
+    "Display/change fff value", "value"},
 
-	{"test", test_handler, "i", 0,
-	NULL,
-	"Test the system", "p1"},
+    {"test", test_handler, "i", 0,
+    NULL,
+    "Test the system", "p1"},
 
-	{"printargs", emci_printargs_handler, "ssssssss", 8,
-	NULL,
-	"Print all arguments passed", NULL},
+    {"printargs", emci_printargs_handler, "ssssssss", 8,
+    NULL,
+    "Print all arguments passed", NULL},
 
-	{"help", emci_help_handler, "s", 1,
-	NULL,
-	"Display this message", "cmd"}
+    {"help", emci_help_handler, "s", 1,
+    NULL,
+    "Display this message", "cmd"}
 };
 
 const uint_fast8_t cmd_array_length = (sizeof(cmd_array) / sizeof(emci_command_t));
 
 void emci_prompt(void)
 {
-	EMCI_PRINTF("emci[%u]>", cmd_var_setpoint);
+    EMCI_PRINTF("emci[%u]>", cmd_var_setpoint);
 }
 
 emci_status_t about_handler(uint8_t argc, emci_arg_t *argv, emci_env_t *env)
 {
-	EMCI_PRINTF("Test Device v1.0.0" EMCI_ENDL);
-	env->resp.msg = "Frequency cannot be measured";
-	return (emci_status_t)APP_FREQ_ERROR;
+    EMCI_PRINTF("Test Device v1.0.0" EMCI_ENDL);
+    env->resp.msg = "Frequency cannot be measured";
+    return (emci_status_t)APP_FREQ_ERROR;
 }
 
 emci_status_t test_handler(uint8_t argc, emci_arg_t *argv, emci_env_t *env)
 {
-	if (argv[1].i > 1000)
-	{
-		env->resp.param = 1;
-		return EMCI_STATUS_ARG_TOO_HIGH;
-	}
-	else if (argv[1].i < 500)
-	{
-		env->resp.param = 1;
-		return EMCI_STATUS_ARG_TOO_LOW;
-	}
+    if (argv[1].i > 1000)
+    {
+        env->resp.param = 1;
+        return EMCI_STATUS_ARG_TOO_HIGH;
+    }
+    else if (argv[1].i < 500)
+    {
+        env->resp.param = 1;
+        return EMCI_STATUS_ARG_TOO_LOW;
+    }
 
-	EMCI_PRINTF("test_handler: %d" EMCI_ENDL, argv[1].i);
-	return EMCI_STATUS_OK;
+    EMCI_PRINTF("test_handler: %d" EMCI_ENDL, argv[1].i);
+    return EMCI_STATUS_OK;
 }
 
 const char *emci_app_status_message(emci_status_t status)
 {
-	switch ((app_status_t)status)
-	{
-		case APP_OK: return "OK"; break;
-		case APP_IO_ERROR: return "IO Error"; break;
-		case APP_FREQ_ERROR: return "Frequency Error"; break;
-		default: return "?"; break;
-	}
+    switch ((app_status_t)status)
+    {
+        case APP_OK: return "OK"; break;
+        case APP_IO_ERROR: return "IO Error"; break;
+        case APP_FREQ_ERROR: return "Frequency Error"; break;
+        default: return "?"; break;
+    }
 }
--- include/emci_parser.h
+++ include/emci_parser.h
@@ -5,30 +5,30 @@
 
 typedef struct emci_response_tag
 {
-	emci_status_t status;
-	uint8_t param;
-	char *msg;
+    emci_status_t status;
+    uint8_t param;
+    char *msg;
 } emci_response_t;
 
 struct emci_env_tag;
 
 typedef struct emci_command_tag
 {
-	const char *name;
-	emci_status_t (*handler)(uint8_t argc, emci_arg_t *argv, struct emci_env_tag *env);
-	const char *arg_types;
-	uint8_t arg_optional;
-	void *extra;
-	const char *cmd_dscr;
-	const char *arg_dscr;
+    const char *name;
+    emci_status_t (*handler)(uint8_t argc, emci_arg_t *argv, struct emci_env_tag *env);
+    const char *arg_types;
+    uint8_t arg_optional;
+    void *extra;
+    const char *cmd_dscr;
+    const char *arg_dscr;
 } emci_command_t;
 
 typedef struct emci_env_tag
 {
-	char cmd_buffer[EMCI_MAX_LINE_LENGTH+1]; // plus 1 for '\0'
-	emci_arg_t arg_buffer[EMCI_MAX_ARGS+1]; // plus 1 for command name
-	emci_command_t *cmd;
-	emci_response_t resp;
+    char cmd_buffer[EMCI_MAX_LINE_LENGTH+1]; // plus 1 for '\0'
+    emci_arg_t arg_buffer[EMCI_MAX_ARGS+1]; // plus 1 for command name
+    emci_command_t *cmd;
+    emci_response_t resp;
 } emci_env_t;
 
 void emci_main_loop(emci_env_t *env);
